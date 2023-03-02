3234621d774539ac821b1bd3c91c30efd85f0961
TayyabKM <109720981+TayyabKM@users.noreply.github.com>
2023-03-02 13:10:47 +0100

    Create todo list

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+  <header style={{
+    fontSize: '3rem',
+    fontWeight: '600',
+    textAlign: 'center',
+    padding: '50px',
+    lineHeight: '1.5em',
+  }} >
+    <h1>To-Do</h1>
+  </header>
+);
+
+export default Header;
